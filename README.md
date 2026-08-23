a8e847abecaef4315a24514a73a4a3f245a2317b
martins-psl <274044756+martins-psl@users.noreply.github.com>
2026-08-23 15:30:55 +0100

    Initial co.podset:core 0.1.0

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# podset-maven
+# co.podset:core
+
+Java library published by PODSET LTD.
+
+Dependency:
+
+  groupId:    co.podset
+  artifactId: core
+  version:    0.1.0
+
+https://podset.co
---
+++ src/main/java/co/podset/core/Podset.java
@@ -0,0 +1,11 @@
+package co.podset.core;
+
+/**
+ * Identity marker for the co.podset namespace.
+ */
+public final class Podset {
+    public static final String NAME = "PODSET";
+    public static final String VERSION = "0.1.0";
+
+    private Podset() {}
+}
